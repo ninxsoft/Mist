df01adb30992997c9cbacc940212e2d0665e0cb3
Nindi Gill <nindi@ninxsoft.com>
2021-09-08 12:28:47 +1000

    Base SwiftUI structure

---
+++ Mist/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Mist
+//
+//  Created by Nindi Gill on 8/9/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Mist/MistApp.swift
@@ -0,0 +1,17 @@
+//
+//  MistApp.swift
+//  Mist
+//
+//  Created by Nindi Gill on 8/9/21.
+//
+
+import SwiftUI
+
+@main
+struct MistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
