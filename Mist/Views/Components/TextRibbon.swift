e7d76713b45f42cb708a6c973c9731837e38d347
Nindi Gill <nindi@ninxsoft.com>
2023-06-24 19:09:25 +1000

    Update TextRibbon.swift

--- Mist/Views/Components/TextRibbon.swift
+++ Mist/Views/Components/TextRibbon.swift
@@ -10,14 +10,12 @@ import SwiftUI
 struct TextRibbon: View {
     var title: String
     var length: CGFloat
-    private let degrees: CGFloat = -45
 
     var body: some View {
         Text(title)
             .foregroundColor(.white)
             .frame(width: length)
             .background(Color.accentColor)
-            .rotationEffect(.degrees(degrees))
             .mask(Circle().frame(width: length, height: length))
     }
 }
