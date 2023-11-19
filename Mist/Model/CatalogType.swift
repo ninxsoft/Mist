f6bfc8d59bd02ff6ba5ef16b8b52df9599bd1581
Nindi Gill <nindi@ninxsoft.com>
2023-11-19 19:43:51 +1100

    SwiftLint remove superfluous line_length

--- Mist/Model/CatalogType.swift
+++ Mist/Model/CatalogType.swift
@@ -47,10 +47,8 @@ enum CatalogType: String, CaseIterable, Comparable, Decodable {
                 // swiftlint:disable:next line_length
                 "https://swscan.apple.com/content/catalogs/others/index-14customerseed-14-13-12-10.16-10.15-10.14-10.13-10.12-10.11-10.10-10.9-mountainlion-lion-snowleopard-leopard.merged-1.sucatalog.gz"
             case .developer:
-                // swiftlint:disable:next line_length
                 "https://swscan.apple.com/content/catalogs/others/index-14seed-14-13-12-10.16-10.15-10.14-10.13-10.12-10.11-10.10-10.9-mountainlion-lion-snowleopard-leopard.merged-1.sucatalog.gz"
             case .public:
-                // swiftlint:disable:next line_length
                 "https://swscan.apple.com/content/catalogs/others/index-14beta-14-13-12-10.16-10.15-10.14-10.13-10.12-10.11-10.10-10.9-mountainlion-lion-snowleopard-leopard.merged-1.sucatalog.gz"
             }
         case .ventura:
@@ -58,7 +56,6 @@ enum CatalogType: String, CaseIterable, Comparable, Decodable {
             case .standard:
                 "https://swscan.apple.com/content/catalogs/others/index-13-12-10.16-10.15-10.14-10.13-10.12-10.11-10.10-10.9-mountainlion-lion-snowleopard-leopard.merged-1.sucatalog.gz"
             case .customer:
-                // swiftlint:disable:next line_length
                 "https://swscan.apple.com/content/catalogs/others/index-13customerseed-13-12-10.16-10.15-10.14-10.13-10.12-10.11-10.10-10.9-mountainlion-lion-snowleopard-leopard.merged-1.sucatalog.gz"
             case .developer:
                 "https://swscan.apple.com/content/catalogs/others/index-13seed-13-12-10.16-10.15-10.14-10.13-10.12-10.11-10.10-10.9-mountainlion-lion-snowleopard-leopard.merged-1.sucatalog.gz"
@@ -70,7 +67,6 @@ enum CatalogType: String, CaseIterable, Comparable, Decodable {
             case .standard:
                 "https://swscan.apple.com/content/catalogs/others/index-12-10.16-10.15-10.14-10.13-10.12-10.11-10.10-10.9-mountainlion-lion-snowleopard-leopard.merged-1.sucatalog.gz"
             case .customer:
-                // swiftlint:disable:next line_length
                 "https://swscan.apple.com/content/catalogs/others/index-12customerseed-12-10.16-10.15-10.14-10.13-10.12-10.11-10.10-10.9-mountainlion-lion-snowleopard-leopard.merged-1.sucatalog.gz"
             case .developer:
                 "https://swscan.apple.com/content/catalogs/others/index-12seed-12-10.16-10.15-10.14-10.13-10.12-10.11-10.10-10.9-mountainlion-lion-snowleopard-leopard.merged-1.sucatalog.gz"
@@ -82,7 +78,6 @@ enum CatalogType: String, CaseIterable, Comparable, Decodable {
             case .standard:
                 "https://swscan.apple.com/content/catalogs/others/index-10.16-10.15-10.14-10.13-10.12-10.11-10.10-10.9-mountainlion-lion-snowleopard-leopard.merged-1.sucatalog.gz"
             case .customer:
-                // swiftlint:disable:next line_length
                 "https://swscan.apple.com/content/catalogs/others/index-10.16customerseed-10.16-10.15-10.14-10.13-10.12-10.11-10.10-10.9-mountainlion-lion-snowleopard-leopard.merged-1.sucatalog.gz"
             case .developer:
                 "https://swscan.apple.com/content/catalogs/others/index-10.16seed-10.16-10.15-10.14-10.13-10.12-10.11-10.10-10.9-mountainlion-lion-snowleopard-leopard.merged-1.sucatalog.gz"
