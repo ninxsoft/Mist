71103f74f0b029032712e35103abb664873d88df
Nindi Gill <nindi@ninxsoft.com>
2023-11-19 19:45:11 +1100

    SwiftFormat consecutiveSpaces

--- Mist/Helpers/TaskManager.swift
+++ Mist/Helpers/TaskManager.swift
@@ -465,7 +465,7 @@ class TaskManager: ObservableObject {
             ]
         } else {
             let installESDURL: URL = installer.temporaryInstallerURL.appendingPathComponent("Contents/SharedSupport/InstallESD.dmg")
-            return  [
+            return [
                 MistTask(type: .convert, description: "Installer Disk Image to ISO") {
                     try await ISOConverter.convert(installESDURL, destination: temporaryCDRURL)
                 },
